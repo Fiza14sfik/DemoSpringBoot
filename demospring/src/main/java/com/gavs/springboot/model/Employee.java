2cf8c27b5ae89eab15621396219719fceb8c78ae
Fiza14sfik <89029825+Fiza14sfik@users.noreply.github.com>
2022-10-31 22:26:32 +0530

    Update Employee.java

--- demospring/src/main/java/com/gavs/springboot/model/Employee.java
+++ demospring/src/main/java/com/gavs/springboot/model/Employee.java
@@ -1,8 +1,30 @@
 package com.gavs.springboot.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee_details")
 
 public class Employee {
+	@Id
+	@Column(name="id")
 	private int id;
+	
+	@Column(name="name")
 	private String name;
+	
+	@Column(name="dept")
+	private String dept;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="salary")
+	private Long salary;
 	
 	public String getName() {
 		return name;
@@ -16,6 +38,23 @@ public class Employee {
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
 	
 }
